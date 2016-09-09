4ae79b978e485495ffc28d909bb44b5bd915c071
Windsor Kitaka <windsor@playdribble.com>
2016-09-09 19:48:51 +0100

    added back looper

---
+++ Assets/BG_Looper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG_Looper : MonoBehaviour {
+	int numBGPanels = 6;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log ("ColliderTriggered: " + collider.name);
+
+		float withOfBGObject = ((BoxCollider2D)collider).size.x;
+		Vector3 pos = collider.transform.position;
+		pos.x += withOfBGObject * numBGPanels - withOfBGObject/2f;
+
+		collider.transform.position = pos;
+	}
+}

767cb8f5e683c399f21abe8d73a42a87f2b090cb
Windsor Kitaka <windsor@playdribble.com>
2016-09-10 15:49:09 +0100

    background mover

---
+++ Assets/BK_GroundMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BK_GroundMover : MonoBehaviour {
+
+	float speed = 1.4f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
